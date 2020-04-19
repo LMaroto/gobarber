fac5ec0c2d16dd20bcceda8251d7d38114e600be
LMaroto <lucasmarotoga@gmail.com>
2020-04-19 19:02:46 -0400

    Estilizando autenticação

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
   return (
-    <div />
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
   );
 }
