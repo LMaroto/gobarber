95d0c321a00f868b0463b3ccf820c6b7ebef0325
LMaroto <lucasmarotoga@gmail.com>
2020-04-19 23:05:09 -0400

    Configurando unform

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form } from '@unform/web';
+import Input from '~/components/Input';
 import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input name="email" type="email" placeholder="Seu e-mail" />
-        <input name="password" type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
