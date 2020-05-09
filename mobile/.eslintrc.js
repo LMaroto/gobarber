45f297d9ebe6ac893366c2f8b81d24e40b955a15
LMaroto <lucasmarotoga@gmail.com>
2020-05-09 17:36:31 -0400

    Configurando root import

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    camelcase: 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    'prefer-object-spread': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react/jsx-props-no-spreading': ['off'],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
+};
